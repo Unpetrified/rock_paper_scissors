a50a00a52654d9218039414514f9d44eb1b5a5c1
Obiefuna Kingsley <kingsleyobiefuna01@gmail.com>
2024-06-16 22:23:14 +0200

    created the logic for each round and the round winner

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let userScore = 0,
-    comScore = 0;
+    comScore = 0,
+    round = 1;
 
 function comPick() {
     
@@ -14,3 +15,52 @@ function userPick() {
     let userPick = (prompt("Rock, Paper, Scissors: ")).toLowerCase();
     return userPick
 }
+
+function play(com, user) {
+    console.log("Score ->", "User: " +userScore, "Computer: " +comScore);
+    console.log("You played: " + user, "Computer played: " + com);
+    if (com === user) {
+        console.log("Tie");
+        return
+    } else if (com === "rock" && user === "paper") {
+        console.log("Paper beats Rock!");
+        roundWinner("user");
+        return
+    } else if (com === "paper" && user === "scissors") {
+        console.log("Scissors beats Paper!");
+        roundWinner("user");
+        return
+    } else if (com === "scissors" && user === "rock") {
+        console.log("Rock beats Scissors!");
+        roundWinner("user");
+        return
+    } else if (user === "rock" && com === "paper") {
+        console.log("Paper beats Rock!");
+        roundWinner("com");
+        return
+    } else if (user === "paper" && com === "scissors") {
+        console.log("Scissors beats Paper!");
+        roundWinner("com");
+        return
+    } else if (user === "scissors" && com === "rock") {
+        console.log("Rock beats Scissors!");
+        roundWinner("com");
+        return
+    }
+}
+
+function roundWinner(winner) {
+    if (winner === "user") {
+        userScore++;
+        console.log("You win this round!");
+        console.log("Score ->", "User: " +userScore, "Computer: " +comScore);
+        return
+    }
+
+    comScore++;
+    console.log("I win this round!");
+    console.log("Score ->", "User: " +userScore, "Computer: " +comScore);
+    return
+}
+
+play("rock", "scissors")
