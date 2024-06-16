d57cdd051bad008b6fa0819ccba4639eda129b09
Obiefuna Kingsley <kingsleyobiefuna01@gmail.com>
2024-06-16 22:40:00 +0200

    created the rounds logic

--- script.js
+++ script.js
@@ -17,33 +17,38 @@ function userPick() {
 }
 
 function play(com, user) {
-    console.log("Score ->", "User: " +userScore, "Computer: " +comScore);
+    if (round > 1) {
+        console.log("Round " + round);
+    } else {
+        console.log("Round 1");
+    }
     console.log("You played: " + user, "Computer played: " + com);
     if (com === user) {
         console.log("Tie");
+        play(comPick(), userPick())
         return
     } else if (com === "rock" && user === "paper") {
-        console.log("Paper beats Rock!");
+        console.log("Paper covers Rock!");
         roundWinner("user");
         return
     } else if (com === "paper" && user === "scissors") {
-        console.log("Scissors beats Paper!");
+        console.log("Scissors cuts Paper!");
         roundWinner("user");
         return
     } else if (com === "scissors" && user === "rock") {
-        console.log("Rock beats Scissors!");
+        console.log("Rock smashes Scissors!");
         roundWinner("user");
         return
     } else if (user === "rock" && com === "paper") {
-        console.log("Paper beats Rock!");
+        console.log("Paper covers Rock!");
         roundWinner("com");
         return
     } else if (user === "paper" && com === "scissors") {
-        console.log("Scissors beats Paper!");
+        console.log("Scissors cuts Paper!");
         roundWinner("com");
         return
     } else if (user === "scissors" && com === "rock") {
-        console.log("Rock beats Scissors!");
+        console.log("Rock smashes Scissors!");
         roundWinner("com");
         return
     }
@@ -63,4 +68,18 @@ function roundWinner(winner) {
     return
 }
 
-play("rock", "scissors")
+function playRound() {
+    while (round <= 5) {
+        play(comPick(), userPick())
+        round++
+    }
+
+    if (userScore > comScore) {
+        console.log("You win!");
+        return
+    }
+
+    console.log("!!!I win!!!");
+}
+
+playRound()
