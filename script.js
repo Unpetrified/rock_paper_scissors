e6262aefc566eb643d9579b2d593fd282b850a89
Obiefuna Kingsley <kingsleyobiefuna01@gmail.com>
2024-06-19 17:45:09 +0200

    migrated the code from console to ui. Discovered a bug where the restart button does not come up after the game is finished.

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let userScore = 0,
     comScore = 0,
     round = 0,
-    tie = false;
+    tie = false,
+    winner = document.querySelector(".winner"),
+    scoreBoard = document.querySelector(".score"),
+    roundWinnerDisplay = document.querySelector(".round-winner"),
+    playerOne = document.querySelector(".player-one"),
+    playerTwo = document.querySelector(".player-two"),
+    reason = document.querySelector(".reason"),
+    roundDisplay = document.querySelector(".round");
 
 let buttons = document.querySelectorAll(".rock-paper-scissors");
 
@@ -20,6 +27,8 @@ function comPick() {
 
 function userPick(e) {
     
+    if (round + 1> 5) return
+
     tie ? tie = false : round++;
     
     let userPick = e.target.getAttribute("id");
@@ -30,11 +39,12 @@ function userPick(e) {
 function checkRound() {
 
     if (round + 1 > 5) {
+
         if (userScore > comScore) {
-            console.log("You win");
+            winner.textContent = "!!!You Win!!!"
             return
         }
-        console.log("I win");
+        winner.textContent = "!!!I Win !!!"
 
         let resetBtn = document.createElement("button");
         resetBtn.textContent = "Play Again";
@@ -51,59 +61,98 @@ function reset(e) {
     comScore = 0;
     round = 0;
     tie = false;
+    winner.textContent = "";
 
     e.target.remove();
 }
 
-function playRound(com, user) {
-    if (round > 1) {
-        console.log("Round " + round);
-    } else {
-        console.log("Round 1");
-    }
-    console.log("You played: " + user, "Computer played: " + com);
-    if (com === user) {
-        tie = true;
-        console.log("Tie");
-        return
-    } else if (com === "rock" && user === "paper") {
-        console.log("Paper covers Rock!");
-        roundWinner("user");
-        return
-    } else if (com === "paper" && user === "scissors") {
-        console.log("Scissors cuts Paper!");
-        roundWinner("user");
-        return
-    } else if (com === "scissors" && user === "rock") {
-        console.log("Rock smashes Scissors!");
-        roundWinner("user");
-        return
-    } else if (user === "rock" && com === "paper") {
-        console.log("Paper covers Rock!");
-        roundWinner("com");
-        return
-    } else if (user === "paper" && com === "scissors") {
-        console.log("Scissors cuts Paper!");
-        roundWinner("com");
-        return
-    } else if (user === "scissors" && com === "rock") {
-        console.log("Rock smashes Scissors!");
-        roundWinner("com");
-        return
+function showPlay(player1, player2) {
+    playerOne.textContent = "You played: " + player1;
+    playerTwo.textContent = "Computer played: " + player2;
+}
+
+function showRoundWinner(winner) {
+    switch (winner) {
+        case "tie":
+            roundWinnerDisplay.textContent = "Tie";
+            break;
+
+        case "com":
+            roundWinnerDisplay.textContent = "I win this round!";
+            break;
+
+        default:
+            roundWinnerDisplay.textContent = "You win this round!";
+            break;
     }
 }
 
+function gameLogic(choiceOne=null, choiceTwo=null) {
+
+    if (!choiceOne) {
+        reason.textContent = "";
+    }
+
+    if (choiceOne === "rock" && choiceTwo === "paper") {
+        reason.textContent = "Paper covers Rock!";
+    } else if (choiceOne === "paper" && choiceTwo === "scissors") {
+        reason.textContent = "Scissors cuts Paper!";
+    } else if (choiceOne === "scissors" && choiceTwo === "rock") {
+        reason.textContent = "Rock smashes Scissors!";
+    }
+
+
+}
+
+function playRound(com, user) {
+    if (round > 1) {
+        roundDisplay.textContent = "Round " + round;
+    } else {
+        roundDisplay.textContent = "Round 1"
+    }
+    showPlay(user, com);
+    if (com === user) {
+        tie = true;
+        showRoundWinner("tie");
+        gameLogic();
+        return
+    } else if (com === "rock" && user === "paper") {
+        gameLogic(com, user)
+        roundWinner("user");
+        return
+    } else if (com === "paper" && user === "scissors") {
+        gameLogic(com, user)
+        roundWinner("user");
+        return
+    } else if (com === "scissors" && user === "rock") {
+        gameLogic(com, user)
+        roundWinner("user");
+        return
+    } else if (user === "rock" && com === "paper") {
+        gameLogic(user, com);
+        
+    } else if (user === "paper" && com === "scissors") {
+        gameLogic(user, com);
+        
+    } else if (user === "scissors" && com === "rock") {
+        gameLogic(user, com);
+        
+    }
+
+    roundWinner("com");
+}
+
 function roundWinner(winner) {
+    showRoundWinner(winner);
+
     if (winner === "user") {
         userScore++;
-        console.log("You win this round!");
-        console.log("Score ->", "User: " +userScore, "Computer: " +comScore);
+        scoreBoard.textContent = "User: " +userScore + " Computer: " +comScore;
         return
     }
 
     comScore++;
-    console.log("I win this round!");
-    console.log("Score ->", "User: " +userScore, "Computer: " +comScore);
+    scoreBoard.textContent = "User: " +userScore + " Computer: " +comScore;
     return
 }
 
